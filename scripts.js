1229cf5a23754483015bd9f71755bbc135b67b98
Montaz Meah <montaz.meah@mrm.com>
2021-06-10 22:28:44 -0400

    Created a first version that uses a while loop.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,26 @@
 "use strict";
 
-console.log("something");
+/* ----- INSTRUCTIONS -----
+
+Extended Challenge / Second Exercise:
+Given any string, use a loop to add padding (extra spaces) to the front to make the string ten characters long. For example, given the string “planet”, log “    planet” (add four spaces), and for the string “headlamp”, log “  headlamp” (add two spaces).
+
+*/
+
+/* version 1 - basic */
+
+let example1 = "planet";
+let example2 = "headlamp";
+
+// using a "while" loop
+
+while(example1.length < 10) {
+    example1 = " " + example1; // add a space before it.
+    console.log(example1); // so you can see it happening.
+}
+
+
+while(example2.length < 10) {
+    example2 = " " + example2; // add a space before it.
+    console.log(example2); // so you can see it happening.
+}
