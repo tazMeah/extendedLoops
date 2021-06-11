7803dd42cd27aade7f67b41e5ded514c0693303c
Montaz Meah <montaz.meah@mrm.com>
2021-06-10 23:11:50 -0400

    Added a FOR loop.

--- scripts.js
+++ scripts.js
@@ -7,22 +7,45 @@ Given any string, use a loop to add padding (extra spaces) to the front to make 
 
 */
 
-/* version 1 - basic */
 
-let example1 = "planet";
-let example2 = "headlamp";
 
-// using a "while" loop
+/* WHILE */
+// while it's less than 10 characters long, put a space in front of it.
+{
+    /* our example words */
+    let example1 = "planet";
+    let example2 = "headlamp";
 
-while(example1.length < 10) {
-    example1 = " " + example1; // add a space before it.
-    console.log(example1); // so you can see it happening.
+    while (example1.length < 10) {
+        example1 = " " + example1; // add a space before it.
+    }
+    console.log("WHILE loop:" + example1); // <-- this is the answer here.
+
+
+    while (example2.length < 10) {
+        example2 = " " + example2; // add a space before it.
+    }
+    console.log("WHILE loop:" + example2); // <-- this is the answer here.
 }
-console.log(example1); // <-- this is the answer here.
 
 
-while(example2.length < 10) {
-    example2 = " " + example2; // add a space before it.
-    console.log(example2); // so you can see it happening.
+/* FOR */
+// for the difference in length from 10 to the word's length, add a space before
+{
+    /* our example words */
+    let example1 = "planet";
+    let example2 = "headlamp";
+
+    let differenceInLength1 = 10 - example1.length; // 4
+    let differenceInLength2 = 10 - example2.length; // 2
+
+    for (let i = 0; i < differenceInLength1; i++) {
+        example1 = " " + example1;
+    }
+    console.log("FOR:" + example1);
+
+    for (let i = 0; i < differenceInLength2; i++) {
+        example2 = " " + example2;
+    }
+    console.log("FOR:" + example2);
 }
-console.log(example2); // <-- this is the answer here.
